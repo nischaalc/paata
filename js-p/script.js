2b918274e095b790365844a9cc8219b2267c919a
nischaalc <nc475@drexel.edu>
2014-07-20 20:48:04 +0530

    Added outputs for RGB and hex values

--- js-p/script.js
+++ js-p/script.js
@@ -1,51 +1,81 @@
+var colorRGBA = "";
+var colorRGB = "";
+var count = 0;
+var alpha = 0;
+var hex = "";
+var val = "";
+
+$( document ).ready(function() {
+    colorRGBA = "";
+    colorRGB = "";
+    count = 0;
+    alpha = 0;
+    hex = "";
+    val = "";
+
+    $('.range-slider').each(function() {
+        val = $(this).attr('data-slider');
+        if (count < 2) {
+            colorRGB = colorRGB + val + ',';
+            hex = hex + componentToHex(val);
+        }
+        else {
+            colorRGB = colorRGB + val;
+            hex = hex + componentToHex(val);
+        }
+        count++;
+    });
     
-        $( document ).ready(function() {
-		var color = "";
-		var count = 0;
-        var alpha = 0;
-		
-		$('.range-slider').each(function() {
-            if (count < 3) {
-				color = color + $(this).attr('data-slider') + ',';
-			}
-            
-            else {
-                alpha = $(this).attr('data-slider');
-            }
-			count++;
-        });
-         
-		color = 'rgba(' + color + ',' + alpha + ')';
-		$('body').css( "background", color);
-	});
-	
-	function componentToHex(c) {
-		var hex = c.toString(16);
-		return hex.length == 1 ? "0" + hex : hex;
-	}
+    colorRGB = 'rgb(' + colorRGB + ')';
+    hex = '#' + hex;
+    $('body').css( "background", colorRGB);
+    displayVal(colorRGB, hex);
+});
 
-	function rgbToHex(r, g, b) {
-		var color = "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-		//document.body.style.background = color;
-	}
+function componentToHex(c) {
+    var hex = parseInt(c).toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
 
-	$('[data-slider]').on('change.fndtn.slider', function(){
-		
-		var color = "";
-		var count = 0;
-        var alpha = 0;
-        
-		$('.range-slider').each(function() {
-            if (count < 3) {
-				color = color + $(this).attr('data-slider') + ',';
-			}
-            
-            else {
-                alpha = $(this).attr('data-slider');
-            }
-			count++;
-        });
+function rgbToHex(r,g,b) {
+    var color = "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
 
-		color = 'rgba(' + color + alpha + ')';
-		$('body').css( "background", color);
-	});
+function displayVal(rgb, hex, rgba) {
+    
+    document.getElementById('rgb-val').innerHTML = rgb;
+    document.getElementById('hex-val').innerHTML = hex;
+}
+
+$('[data-slider]').on('change.fndtn.slider', function(){
+
+    colorRGBA = "";
+    colorRGB = "";
+    count = 0;
+    alpha = 0;
+    hex = "";
+    val = "";
+
+    $('.range-slider').each(function() {
+        val = $(this).attr('data-slider');
+        if (count < 2) {
+            colorRGB = colorRGB + val + ',';
+            hex = hex + componentToHex(val);
+        }
+        else {
+            colorRGB = colorRGB + val;
+            hex = hex + componentToHex(val);
+        }
+        count++;
+    });
+    
+    colorRGB = 'rgb(' + colorRGB + ')';
+    hex = '#' + hex;
+    $('body').css( "background", colorRGB);
+    displayVal(colorRGB, hex);
+});
+
+function getComplement(color) {
+    
+}
+
