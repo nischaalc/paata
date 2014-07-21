48158012ffe4efc09b498650ee3f7bff052d4258
Nischaal Cooray <nc475@drexel.edu>
2014-07-21 13:40:16 +0530

    Update script.js

--- js-p/script.js
+++ js-p/script.js
@@ -1,3 +1,4 @@
+//---Constants---
 var colorRGBA = "";
 var colorRGB = "";
 var count = 0;
@@ -5,15 +6,11 @@ var alpha = 0;
 var hex = "";
 var val = "";
 
+/*
+ - Set initial values/colors for when page is loaded
+ */
 $( document ).ready(function() {
-    colorRGBA = "";
-    colorRGB = "";
-    count = 0;
-    alpha = 0;
-    hex = "";
-    val = "";
-
-    $('.range-slider').each(function() {
+     $('.range-slider').each(function() {
         val = $(this).attr('data-slider');
         if (count < 2) {
             colorRGB = colorRGB + val + ',';
@@ -30,26 +27,37 @@ $( document ).ready(function() {
     hex = '#' + hex;
     $('body').css( "background", colorRGB);
 	$('#title').css( "color", '#000000');
-    displayVal(colorRGB, hex);
+    displayVal(colorRGB, hex, compColor);
 });
 
+/*
+ - Convert integer values from sliders to corresponding hex values
+ - c: the integer to be converted
+ - return: the hex value of the 'c'
+ */
 function componentToHex(c) {
     var hex = parseInt(c).toString(16);
     return hex.length == 1 ? "0" + hex : hex;
 }
 
-function rgbToHex(r,g,b) {
-    var color = "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
-
-function displayVal(rgb, hex, rgba) {
+/*
+ - Display the RGB, hex and complementary color codes
+ - rgb: the RGB color code
+ - hex: the hex color code
+ - comp: the hex code of the complementary color
+ */
+function displayVal(rgb, hex, comp) {
     
     document.getElementById('rgb-val').innerHTML = rgb;
     document.getElementById('hex-val').innerHTML = hex;
+	document.getElementById('comp-val').innerHTML = '#' + comp;
 }
 
+/*
+ - Change the background color and values onscreen dynamically as the user manipulates the sliders
+ */
 $('[data-slider]').on('change.fndtn.slider', function(){
-
+	//---Initialize the variables---
     colorRGBA = "";
     colorRGB = "";
     count = 0;
@@ -75,5 +83,8 @@ $('[data-slider]').on('change.fndtn.slider', function(){
     hex = '#' + hex;
     $('body').css( "background", colorRGB);
 	$('#title').css("color", compColor);
-    displayVal(colorRGB, hex);
+	$('#comp-val').css("color", hex);
+	$('.comp-val-cont').css("background-color", compColor);
+	
+    displayVal(colorRGB, hex, compColor);
 });
